35788e710cda924714c8aad605c9cbd3bebc6d36
vica.bats <vitoria_batistoti@hotmail.com>
2025-02-27 21:37:43 -0300

    feat: add angular

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { HomePage } from './features/home/pages/home-page/home-page';
+
+export const routes: Routes = [
+    { path: '', component: HomePage },  
+];
