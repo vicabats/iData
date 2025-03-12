1b5d3f470b32fbb3272422719e9e0ad5c5a5319e
vicabats <vitoria.abreu@caju.com.br>
2025-03-11 21:24:10 -0300

    feat: update input component

--- frontend/src/app/features/signin/pages/sign-in-patient-page/sign-in-patient-page.ts
+++ frontend/src/app/features/signin/pages/sign-in-patient-page/sign-in-patient-page.ts
@@ -1,12 +1,67 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { InputComponent } from '../../../../shared/components/form-components/input/input.component';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-sign-in-patient-page',
-  imports: [InputComponent],
+  imports: [InputComponent, FormsModule, ReactiveFormsModule, MatInputModule],
   templateUrl: './sign-in-patient-page.html',
-  styleUrl: './sign-in-patient-page.css'
+  styleUrl: './sign-in-patient-page.css',
 })
 export class SignInPatientPage {
+  patientForm: FormGroup = new FormGroup({});
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.patientForm = this.fb.group({
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^((?:[^\s]+(?:\s+|$)){2,})$/),
+        ],
+      ],
+      // cpf: ['', [Validators.required, this.cpfValidator]],
+    });
+  }
+
+  // cpfValidator(control: FormControl): { [key: string]: boolean } | null {
+  //   const cpf = control.value;
+  //   const cpfPattern = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+  //   if (cpf && !cpfPattern.test(cpf)) {
+  //     return { invalidCpf: true };
+  //   }
+  //   return null;
+  // }
+
+  getErrorMessage(controlName: string): string {
+    const control = this.patientForm.get(controlName);
+    if (control && control.touched && control.invalid) {
+      if (control.errors?.['required']) {
+        return 'Este campo é obrigatório';
+      } else if (control.errors?.['pattern']) {
+        console.log(control.errors);
+        return 'Formato inválido';
+      }
+    }
+    return '';
+  }
+
+  trySignUp(): void {
+    if (this.patientForm.valid) {
+      window.alert('Cadastro realizado!');
+      this.patientForm.reset();
+    } else {
+      window.alert('Por favor, corrija os campos do formulário.');
+    }
+  }
 }
--- frontend/src/app/shared/components/form-components/input/input.component.ts
+++ frontend/src/app/shared/components/form-components/input/input.component.ts
@@ -1,20 +1,88 @@
+import { Component, Input, forwardRef } from '@angular/core';
+import {
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+  NG_VALIDATORS,
+  Validator,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective } from 'ngx-mask';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-input',
+  standalone: true,
   imports: [
     CommonModule,
     MatInputModule,
-    FormsModule,
+    MatFormFieldModule,
     ReactiveFormsModule,
+    NgxMaskDirective,
   ],
   templateUrl: './input.component.html',
-  styleUrl: './input.component.css'
+  styleUrls: ['./input.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor, Validator {
   @Input() label!: string;
   @Input() type: string = 'text';
+  @Input() placeholder: string = '';
+  @Input() errorMessage: string = 'Campo obrigatório';
+  @Input() mask?: string;
+
+  private _value: any;
+  private onChange: (value: any) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  get value(): any {
+    return this._value;
+  }
+
+  set value(val: any) {
+    this._value = val;
+    this.onChange(val);
+    this.onTouched();
+  }
+
+  writeValue(value: any): void {
+    this._value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  validate(control: AbstractControl): ValidationErrors | null {
+    return control.valid
+      ? null
+      : { invalidForm: { valid: false, message: 'Invalid form' } };
+  }
+
+  onInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.value = input.value;
+  }
+
+  get showError(): boolean {
+    return this.errorMessage !== '' && this.errorMessage.length > 0;
+  }
 }
