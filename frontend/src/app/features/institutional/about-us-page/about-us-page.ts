d2053eaaa36230cb2806351ec15cd9e8a186bb93
unknown <pdrhmoreira@gmail.com>
2025-03-15 14:48:41 -0300

    feat: add about us page

--- frontend/src/app/features/institutional/about-us-page/about-us-page.ts
+++ frontend/src/app/features/institutional/about-us-page/about-us-page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-about-us-page',
-  imports: [],
+  standalone: true, // Torna o componente independente
+  imports: [MatCardModule], // Importando diretamente o MatCardModule
   templateUrl: './about-us-page.html',
   styleUrl: './about-us-page.css'
 })
-export class AboutUsPage {
-
-}
+export class AboutUsPage { }
