5ce0062984f15ca1aaaa85132038e5b46913777e
unknown <pdrhmoreira@gmail.com>
2025-03-16 18:37:07 -0300

    feat: login page in progress

--- frontend/src/app/features/login/pages/login-page/login-page.ts
+++ frontend/src/app/features/login/pages/login-page/login-page.ts
@@ -1,17 +1,67 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { InputComponent } from '../../../../shared/components/form-components/input/input.component';
+import { 
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    CommonModule,
+    InputComponent,
+  ],
   templateUrl: './login-page.html',
-  styleUrl: './login-page.css'
+  styleUrl: './login-page.css',
 })
 export class LoginPage {
-
-  constructor(private router: Router) { }
-
-  redirectToSignIn(): void {
-    this.router.navigate(['signin']);
+  loginForm: FormGroup = new FormGroup({});
+  
+  constructor(private fb: FormBuilder) {}
+  
+  ngOnInit(): void {
+    this.loginForm = this.fb.group(
+      {
+        cpf: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/),
+          ],
+        ],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(8),
+            Validators.pattern(/^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/),
+          ],
+        ]
+      }
+    );
   }
+  
+  getErrorMessage(controlName: string): string {
+    const control = this.loginForm.get(controlName);
+    if (control && control.touched && control.invalid) {
+      if (control.errors?.['required']) {
+        return 'Este campo é obrigatório';
+      } else if (control.errors?.['pattern']) {
+        return 'Formato inválido';
+      }
+    }
+    return '';
+  }
+  
 }
