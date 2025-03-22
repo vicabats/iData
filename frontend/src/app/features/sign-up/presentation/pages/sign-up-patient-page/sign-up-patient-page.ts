d099703487da04e5bc298fd77c4e33e3333f0e60
unknown <pdrhmoreira@gmail.com>
2025-03-22 18:01:19 -0300

    feat: adicionar separador no formulario de login

--- frontend/src/app/features/sign-up/presentation/pages/sign-up-patient-page/sign-up-patient-page.ts
+++ frontend/src/app/features/sign-up/presentation/pages/sign-up-patient-page/sign-up-patient-page.ts
@@ -10,6 +10,7 @@ import {
   ValidationErrors,
 } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
 import { InputComponent } from '../../../../../shared/components/form-components/input/input.component';
 import { PersonalUser } from '../../../../../shared/types/personal_user';
 import { UserAddress } from '../../../../../shared/types/user_address';
@@ -30,6 +31,7 @@ import {
     FormsModule,
     ReactiveFormsModule,
     MatInputModule,
+    MatDividerModule,
     CommonModule,
     InputComponent,
     TitleCasePipe,
