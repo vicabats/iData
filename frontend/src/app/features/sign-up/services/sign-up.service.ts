2ce077ae0524eb4e115b4ce72d702963a7d6601b
Juliana Haddad <juliana.haddado@gmail.com>
2025-04-11 02:50:15 -0300

    feat: update endpoint api/user/register

--- frontend/src/app/features/sign-up/services/sign-up.service.ts
+++ frontend/src/app/features/sign-up/services/sign-up.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { PersonalUser } from '../../../shared/types/personal_user';
 import { Observable } from 'rxjs';
+import { PersonalUser } from '../../../shared/types/personal_user';
 import { ProfessionalUser } from '../../../shared/types/professional_user';
 
 @Injectable({
@@ -11,13 +11,13 @@ export class SignUpService {
   constructor(private http: HttpClient) {}
 
   registerPersonalUser(user: PersonalUser): Observable<String> {
-    const apiUrl = 'http://localhost:8080/api/user/login?type=personal';
+    const apiUrl = 'http://localhost:8080/api/user/register?type=personal';
 
     return this.http.post<String>(apiUrl, user);
   }
 
   registerProfessionalUser(user: ProfessionalUser): Observable<String> {
-    const apiUrl = 'http://localhost:8080/api/user/login?type=professional';
+    const apiUrl = 'http://localhost:8080/api/user/register?type=professional';
 
     return this.http.post<String>(apiUrl, user);
   }
