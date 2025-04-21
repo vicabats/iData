b5ebf06e94edbf177c7d85fca2c35cf864c2ec3d
Juliana Haddad <juliana.haddado@gmail.com>
2025-04-21 01:22:50 -0300

    Adiciona CrossOrigin a CorsConfiguration

--- backend/idata/src/main/java/com/fatecipiranga/idata/config/CorsConfiguration.java
+++ backend/idata/src/main/java/com/fatecipiranga/idata/config/CorsConfiguration.java
@@ -1,5 +1,7 @@
 package com.fatecipiranga.idata.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,8 +10,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CorsConfiguration.class);
+
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
+        LOGGER.info("Configurando CORS para permitir origens: http://localhost, http://localhost:4200");
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost", "http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
