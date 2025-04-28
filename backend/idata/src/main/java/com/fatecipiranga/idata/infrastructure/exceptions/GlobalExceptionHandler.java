8b41a475f214eb84bd5953ddd314810b847eb275
unknown <pdrhmoreira@gmail.com>
2025-04-27 21:06:37 -0300

    bugfix: localhost connection

--- backend/idata/src/main/java/com/fatecipiranga/idata/infrastructure/exceptions/GlobalExceptionHandler.java
+++ backend/idata/src/main/java/com/fatecipiranga/idata/infrastructure/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.fatecipiranga.idata.infrastructure.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(UserManagementException.class)
     public ResponseEntity<ErrorResponse> handleUserManagementException(UserManagementException ex) {
@@ -16,6 +20,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
+        log.error("Erro não tratado na requisição", ex); // Loga o stack trace
         ErrorResponse errorResponse = new ErrorResponse("Erro interno no servidor", "INTERNAL_SERVER_ERROR");
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
