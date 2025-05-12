25194e2fb70f483a1e2f1fadc9dd4dc83005f35f
vica.bats <vitoria_batistoti@hotmail.com>
2025-05-12 20:15:02 -0300

    refactor: create ProfessionalUserForm

--- frontend/src/app/features/register/presentation/pages/sections/register-professional-content-page/register-professional-content-page.ts
+++ frontend/src/app/features/register/presentation/pages/sections/register-professional-content-page/register-professional-content-page.ts
@@ -1,130 +1,22 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { ProfessionalUser } from '../../../../../../shared/types/professional_user';
-import {
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import {
-  getBirthdateRegexValidator,
-  getCPFRegexValidator,
-  getNameRegexValidator,
-  getPasswordMinLenghtValidator,
-  getPasswordRegexValidator,
-  getPhoneRegexValidator,
-  getZipCodeRegexValidator,
-  passwordMatchValidator,
-} from '../../../helpers/forms-validators';
-import { UserAddress } from '../../../../../../shared/types/user_address';
-import { MatInputModule } from '@angular/material/input';
-import { MatDividerModule } from '@angular/material/divider';
+import { FormGroup } from '@angular/forms';
+
 import { CommonModule } from '@angular/common';
-import { InputComponent } from '../../../../../../shared/components/form-components/input/input.component';
+import { ProfessionalUserFormComponent } from '../../../../../../shared/components/form-components/professional-user-form/professional-user-form.component';
 
 @Component({
   selector: 'app-register-professional-content-page',
-  imports: [
-    FormsModule,
-    ReactiveFormsModule,
-    MatInputModule,
-    MatDividerModule,
-    CommonModule,
-    InputComponent,
-  ],
+  imports: [CommonModule, ProfessionalUserFormComponent],
   templateUrl: './register-professional-content-page.html',
   styleUrl: './register-professional-content-page.css',
 })
-export class RegisterProfessionalContentPage implements OnInit {
+export class RegisterProfessionalContentPage {
   @Output() submitForm = new EventEmitter<ProfessionalUser>();
 
   public professionalForm: FormGroup = new FormGroup({});
 
-  constructor(private formBuilder: FormBuilder) {}
-
-  ngOnInit(): void {
-    this.initializeForm();
-  }
-
-  private initializeForm() {
-    this.professionalForm = this.formBuilder.group(
-      {
-        name: ['', [Validators.required, getNameRegexValidator()]],
-        cpf: ['', [Validators.required, getCPFRegexValidator()]],
-        birthdate: ['', [Validators.required, getBirthdateRegexValidator()]],
-        professionalLicense: ['', [Validators.required]],
-        street: ['', [Validators.required]],
-        addressNumber: ['', [Validators.required]],
-        addressComplement: [''],
-        zipCode: ['', [Validators.required, getZipCodeRegexValidator()]],
-        neighborhood: ['', [Validators.required]],
-        city: ['', [Validators.required]],
-        state: ['', [Validators.required]],
-        phone: ['', [Validators.required, getPhoneRegexValidator()]],
-        email: ['', [Validators.required, Validators.email]],
-        password: [
-          '',
-          [
-            Validators.required,
-            getPasswordMinLenghtValidator(),
-            getPasswordRegexValidator(),
-          ],
-        ],
-        confirmPassword: ['', [Validators.required]],
-      },
-      { validators: passwordMatchValidator }
-    );
-  }
-
-  public getErrorMessage(controlName: string, placeholder?: string): string {
-    const control = this.professionalForm.get(controlName);
-    if (control && control.touched && control.invalid) {
-      if (control.errors?.['required']) {
-        return 'Este campo não pode ficar em branco.';
-      } else if (control.errors?.['pattern']) {
-        return 'Formato inválido. Siga o padrão: ' + placeholder;
-      }
-    }
-    return '';
-  }
-
-  public tryRegister(): void {
-    if (this.professionalForm.valid) {
-      const user = this.getProfessionalUserObject();
-      this.submitForm.emit(user);
-    } else {
-      this.professionalForm.markAllAsTouched();
-    }
-  }
-
-  private getProfessionalUserObject(): ProfessionalUser {
-    const professionalUser: ProfessionalUser = {
-      name: this.professionalForm.get('name')?.value,
-      cpf: this.professionalForm.get('cpf')?.value,
-      professionalLicense: this.professionalForm.get('professionalLicense')
-        ?.value,
-      birthdate: this.professionalForm.get('birthDate')?.value,
-      facility: this.getUserAddress(),
-      phone: this.professionalForm.get('phone')?.value,
-      email: this.professionalForm.get('email')?.value,
-      password: this.professionalForm.get('password')?.value,
-    };
-
-    return professionalUser;
-  }
-
-  private getUserAddress(): UserAddress {
-    const userAddress: UserAddress = {
-      street: this.professionalForm.get('street')?.value,
-      number: this.professionalForm.get('addressNumber')?.value,
-      complement: this.professionalForm.get('addressComplement')?.value,
-      zipCode: this.professionalForm.get('zipCode')?.value,
-      neighborhood: this.professionalForm.get('neighborhood')?.value,
-      city: this.professionalForm.get('city')?.value,
-      state: this.professionalForm.get('state')?.value,
-    };
-
-    return userAddress;
+  public onFormSubmit(user: ProfessionalUser): void {
+    this.submitForm.emit(user);
   }
 }
--- frontend/src/app/shared/components/form-components/professional-user-form/professional-user-form.component.ts
+++ frontend/src/app/shared/components/form-components/professional-user-form/professional-user-form.component.ts
@@ -1,11 +1,139 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ProfessionalUser } from '../../../types/professional_user';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  getBirthdateRegexValidator,
+  getCPFRegexValidator,
+  getNameRegexValidator,
+  getPasswordMinLenghtValidator,
+  getPasswordRegexValidator,
+  getPhoneRegexValidator,
+  getZipCodeRegexValidator,
+  passwordMatchValidator,
+} from '../../../../features/register/presentation/helpers/forms-validators';
+import { UserAddress } from '../../../types/user_address';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import { InputComponent } from '../input/input.component';
 
 @Component({
   selector: 'app-professional-user-form',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatDividerModule,
+    InputComponent,
+  ],
   templateUrl: './professional-user-form.component.html',
-  styleUrl: './professional-user-form.component.css'
+  styleUrl: './professional-user-form.component.css',
 })
-export class ProfessionalUserFormComponent {
+export class ProfessionalUserFormComponent implements OnInit {
+  @Input() initialData: ProfessionalUser | null = null;
+  @Input({ required: true }) mode!: 'register' | 'edit' | 'view';
+  @Input({ required: true }) buttonLabel!: string;
+  @Output() onSubmit = new EventEmitter<ProfessionalUser>();
 
+  public professionalForm: FormGroup = new FormGroup({});
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+    // if (this.initialData) {
+    //   this.populateForm(this.initialData);
+    // }
+    // if (this.mode === 'view') {
+    //   this.personalForm.disable();
+    // }
+  }
+
+  private initializeForm() {
+    this.professionalForm = this.formBuilder.group(
+      {
+        name: ['', [Validators.required, getNameRegexValidator()]],
+        cpf: ['', [Validators.required, getCPFRegexValidator()]],
+        birthdate: ['', [Validators.required, getBirthdateRegexValidator()]],
+        professionalLicense: ['', [Validators.required]],
+        street: ['', [Validators.required]],
+        addressNumber: ['', [Validators.required]],
+        addressComplement: [''],
+        zipCode: ['', [Validators.required, getZipCodeRegexValidator()]],
+        neighborhood: ['', [Validators.required]],
+        city: ['', [Validators.required]],
+        state: ['', [Validators.required]],
+        phone: ['', [Validators.required, getPhoneRegexValidator()]],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            getPasswordMinLenghtValidator(),
+            getPasswordRegexValidator(),
+          ],
+        ],
+        confirmPassword: ['', [Validators.required]],
+      },
+      { validators: passwordMatchValidator }
+    );
+  }
+
+  public getErrorMessage(controlName: string, placeholder?: string): string {
+    const control = this.professionalForm.get(controlName);
+    if (control && control.touched && control.invalid) {
+      if (control.errors?.['required']) {
+        return 'Este campo não pode ficar em branco.';
+      } else if (control.errors?.['pattern']) {
+        return 'Formato inválido. Siga o padrão: ' + placeholder;
+      }
+    }
+    return '';
+  }
+
+  public onSubmitButtonClicked(): void {
+    if (this.professionalForm.valid) {
+      const user = this.getProfessionalUserObject();
+      this.onSubmit.emit(user);
+    } else {
+      this.professionalForm.markAllAsTouched();
+    }
+  }
+
+  private getProfessionalUserObject(): ProfessionalUser {
+    const professionalUser: ProfessionalUser = {
+      name: this.professionalForm.get('name')?.value,
+      cpf: this.professionalForm.get('cpf')?.value,
+      professionalLicense: this.professionalForm.get('professionalLicense')
+        ?.value,
+      birthdate: this.professionalForm.get('birthDate')?.value,
+      facility: this.getUserAddress(),
+      phone: this.professionalForm.get('phone')?.value,
+      email: this.professionalForm.get('email')?.value,
+      password: this.professionalForm.get('password')?.value,
+    };
+
+    return professionalUser;
+  }
+
+  private getUserAddress(): UserAddress {
+    const userAddress: UserAddress = {
+      street: this.professionalForm.get('street')?.value,
+      number: this.professionalForm.get('addressNumber')?.value,
+      complement: this.professionalForm.get('addressComplement')?.value,
+      zipCode: this.professionalForm.get('zipCode')?.value,
+      neighborhood: this.professionalForm.get('neighborhood')?.value,
+      city: this.professionalForm.get('city')?.value,
+      state: this.professionalForm.get('state')?.value,
+    };
+
+    return userAddress;
+  }
 }
