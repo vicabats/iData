32241109f3172d62de496fa9d57cd6b2c7a90319
unknown <pdrhmoreira@gmail.com>
2025-06-09 20:40:05 -0300

    fix: update adm email

--- backend/idata/src/main/java/com/fatecipiranga/idata/config/MailConfig.java
+++ backend/idata/src/main/java/com/fatecipiranga/idata/config/MailConfig.java
@@ -1,11 +1,11 @@
 package com.fatecipiranga.idata.config;
 
+import java.util.Properties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import java.util.Properties;
 
 @Configuration
 public class MailConfig {
@@ -15,8 +15,8 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("josenascimentoprofessional@gmail.com");
-        mailSender.setPassword("jfci wvxt tizz dbvq");
+        mailSender.setUsername("idata.adm@gmail.com");
+        mailSender.setPassword("qwrx ewhc dooc kwsa");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
